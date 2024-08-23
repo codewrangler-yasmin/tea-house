23405e34416c0494c28b298eef98bc5cf67becdb
codewrangler-yasmin <mahdiaayasmin@gmail.com>
2024-08-23 11:11:46 +0600

    Created tailwind CSS workspace for Tea House project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./public/script.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "",
+        secondary: "",
+      },
+      fontFamily: {
+        primary: ['"Manrope", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
